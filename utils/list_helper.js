224313f68a36017377654bb1a6e17ef9eb42c260
Giuliano Faustini <giuliperus@gmail.com>
2023-09-20 10:43:13 +0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,8 +7,23 @@ const dummy = (blogs) => {
   const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
+
+  const favoriteBlog = (blogs) => {
+   if (blogs.length === 0) {
+    return null
+   }
+    let favorite = blogs[0]
+    for (const blog of blogs) {
+        if(blog.likes > favorite.likes)
+        favorite = blog
+        console.log(favorite)
+    }
+    return favorite
+  }
+  
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
   };
