ad0393778320081aa5d4c8ec357e431291fab1a4
Giuliano Faustini <giuliperus@gmail.com>
2023-09-21 11:05:42 +0300

    4.10: Blog list tests, step3. Added test to verify create new blog and save in test database

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -7,6 +7,23 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const initialBlogs = [
+  {
+    title: 'The first thest blog.',
+    author: 'Not me',
+    url: 'www.firstTestBlog.com',
+    like: '23',
+    id: '650bdcf451d9d807739f6f57'
+  },
+  {
+    title: 'The Second Test Blog EFFF',
+    author: 'not me',
+    url: 'www.testthis.com',
+    likes: 45,
+    id: '650bdddf51d9d807739f6f58'
+  }
+]
+
 test('Blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -27,6 +44,25 @@ test('The unique identifier property of the blog posts is named id', async () =>
     expect(blog.id).toBeDefined()
     console.log(blog.id)
   }
+})
+
+
+test('A valid blog can be added to the list in the databse' , async() => {
+const newBlog = {
+  title: "A third test blog is add?",
+  author: "Me",
+  url: "www.thirdtestblog.com",
+  likes: 9999,
+};
+await api
+.post('/api/blogs')
+.send(newBlog)
+.expect(201)
+.expect('Content-Type', /application\/json/)
+const response = await api.get('/api/blogs')
+const titles = response.body.map(r => r.title)
+expect(response.body).toHaveLength(initialBlogs.length + 1)
+expect(titles).toContain('A third test blog is add?')
 })
 
 afterAll(async () => {
