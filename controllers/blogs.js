ffbedcab0f2be98c60338692687d736843848315
Giuliano Faustini <giuliperus@gmail.com>
2023-09-21 09:50:02 +0300

    Updated routes handler in async await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,75 +1,79 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+
+blogsRouter.get("/", async(request, response) => {
+  try {
+    const blog = await Blog.find({}).then((blogs) => {
+      response.json(blogs);
+    });
+  } catch (error) {
+    next(error)
+  }
 });
 
-blogsRouter.post("/", (request, response, next) => {
+
+blogsRouter.post("/", async (request, response, next) => {
   const { body } = request;
-
   console.log("Received POST request with body:", body);
-
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   });
-
-  blog
-    .save()
-    .then((savedBlog) => {
-      response.status(201).json(savedBlog);
-    })
-    .catch((error) => next(error));
+  try {
+    const savedBlog = await blog.save()
+  response.status(201).json(savedBlog);
+  } catch (error) {
+    next(error)
+  }
 });
 
-blogsRouter.delete("/:id", (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+
+blogsRouter.delete("/:id", async(request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end();
+  } catch (error) {
+    next(error)
+  }
 });
 
-blogsRouter.get("/:id", (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then((blog) => {
-      if (blog) {
-        response.json(blog);
-      } else {
-        response.status(404).end();
-        console.error(
-          "Error fetching person by ID. Insert the right ID number."
-        );
-      }
-    })
-    .catch((error) => next(error));
+
+blogsRouter.get("/:id", async(request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog);
+    } else {
+      response.status(404).end();
+      console.error("Error fetching person by ID. Insert the right ID number.");
+    }
+  } catch (error) {
+      next(error)
+  }
 });
 
-blogsRouter.put("/:id", (request, response, next) => {
+
+blogsRouter.put("/:id", async(request, response, next) => {
   const { body } = request;
-
   const updatedBlog = {
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
   };
-
-  Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
-    .then((updatedBlog) => {
-      if (updatedBlog) {
-        response.json(updatedBlog);
-      } else {
-        response.status(404).json({ error: "Blog not found" });
-      }
-    })
-
-    .catch((error) => next(error));
+  try {
+   const updatedBlogDoc = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+    if (updatedBlogDoc) {
+      response.json(updatedBlogDoc);
+    } else {
+      response.status(404).json({ error: "Blog not found" });
+    }
+  } catch(error) {
+    next(error)
+  }
 });
 
 module.exports = blogsRouter;
