66d86beef5713bd28fb76649e9070ece023497f0
Giuliano Faustini <giuliperus@gmail.com>
2023-09-23 18:54:05 +0300

    4.17: bloglist expansion, step5. Expanded functionality for populating users into blogs and blogs into users

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,8 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const usersRouter = require('../controllers/users')
-const User = require('../models/user')
+const User = require('../models/user');
+const { usersInDb } = require("../tests/test_helper");
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({})
@@ -10,37 +11,52 @@ usersRouter.get('/', async (request, response) => {
 
 blogsRouter.get("/", async(request, response) => {
   try {
-    const blog = await Blog.find({}).then((blogs) => {
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
       response.json(blogs);
-    });
   } catch (error) {
     next(error)
   }
 });
 
-
 blogsRouter.post("/", async (request, response, next) => {
   const { body } = request;
   console.log("Received POST request with body:", body);
+
   if (!body.title || !body.url) {
     return response.status(400).json({ error: 'Title and URL are required' });
   }
   if (!body.likes) {
     body.likes = 0;
   }
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  });
+
   try {
-    const savedBlog = await blog.save()
+    
+    const users = await User.findOne({});
+
+    if (!users || users.length === 0) {
+      return response.status(404).json({ error: 'No users found in the database' });
+    }
+
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: users._id, 
+    });
+
+    const savedBlog = await blog.save();
+
+    users.blogs = users.blogs.concat(savedBlog._id);
+    await users.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
     next(error);
   }
 });
+
 
 
 
